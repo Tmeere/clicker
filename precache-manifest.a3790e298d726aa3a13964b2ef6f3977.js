ec5fcc2e46402aca0cd1fee916c2f3f5c56bdb5a
Thomas Meere <thomas.meere@piksel.com>
2019-06-26 11:47:00 +0100

    Updates

---
+++ precache-manifest.a3790e298d726aa3a13964b2ef6f3977.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e4fcdf4c351c55aaa5ea82d45565a4c",
+    "url": "/clicker/index.html"
+  },
+  {
+    "revision": "6c1683f4df2ad730a90a",
+    "url": "/clicker/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/clicker/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "6c1683f4df2ad730a90a",
+    "url": "/clicker/static/js/main.cbcd7f1e.chunk.js"
+  },
+  {
+    "revision": "6e4e176355e4075678fe",
+    "url": "/clicker/static/js/runtime~main.b35d8f1a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "tmeere.github.io/clicker/precache-manifest.25d3cc225c1b9f59727a19e1a90be4f8.js"
+  "/clicker/precache-manifest.a3790e298d726aa3a13964b2ef6f3977.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("tmeere.github.io/clicker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clicker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
