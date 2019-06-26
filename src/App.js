11e90f61a747b6c410e58f1a3d367a9e4b5a3521
Joseph Abell <joseph.abell@piksel.com>
2019-06-26 12:30:55 +0100

    use useInterval to create macros

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
+import useInterval from 'use-interval';
 import './App.css';
 
 function App() {
@@ -6,17 +7,20 @@ function App() {
   const [hasLemonade, setHasLemonade] = useState(false);
   const [hasLemonadePurchaser, setHasLemonadePurchaser] = useState(false);
   const [hasIceCream, setHasIceCream] = useState(false);
+
+  useInterval(() => hasLemonadePurchaser && setMoney(money + 3), 1000);
+
   const items = [
     {
       purchased: hasLemonade,
       purchaseFunction: setHasLemonade,
       purchaserFunction: setHasLemonadePurchaser,
-      purchaserPurchased: hasLemonadePurchaser,
+      hasPurchaser: hasLemonadePurchaser,
       imageUrl: 'lemonade.jpg',
       standPrice: 100,
       purchaserPrice: 50,
       itemPrice: 3,
-      name: 'Lemonade Stand'
+      name: 'Lemonade Stand',
     },
     {
       purchased: hasIceCream,
@@ -24,30 +28,18 @@ function App() {
       imageUrl: 'ice cream.jpg',
       standPrice: 150,
       itemPrice: 10,
-      name: 'Ice Cream Stand'
-    }
+      name: 'Ice Cream Stand',
+    },
   ];
-  
-  useEffect(() => {
-    if (hasLemonadePurchaser) {
-      const purchaseItem = setInterval(() => {
-        setMoney(money + 3);
-      }, 1000);
 
-      return () => {
-        clearInterval(purchaseItem)
-      }
-    }
-  }, [hasLemonadePurchaser, money]);
-  
   const sell = (num = 1) => {
     setMoney(money + num);
-  }
+  };
 
   const purchaseStand = (func, price) => {
     func(true);
     setMoney(money - price);
-  }
+  };
 
   return (
     <div className="App">
@@ -55,19 +47,35 @@ function App() {
         {money}
 
         <ul>
-          {items.map((item) => (
+          {items.map(item => (
             <li>
               {item.purchased && (
-                <img src={item.imageUrl} onClick={() => sell(item.itemPrice)} />
+                <img
+                  src={item.imageUrl}
+                  alt={item.name}
+                  onClick={() => sell(item.itemPrice)}
+                />
               )}
 
               {money >= item.standPrice && !item.purchased && (
-                <button onClick={() => purchaseStand(item.purchaseFunction, item.standPrice)}>Purchase {item.name}</button>
+                <button
+                  onClick={() =>
+                    purchaseStand(item.purchaseFunction, item.standPrice)
+                  }>
+                  Purchase {item.name}
+                </button>
               )}
 
-              {item.purchased && money >= item.purchaserPrice && !item.purchaserPurchased && (
-                <button onClick={() => purchaseStand(item.purchaserFunction, item.purchaserPrice)}>Purchase {item.name} Buyer</button>
-              )}
+              {item.purchased &&
+                money >= item.purchaserPrice &&
+                !item.hasPurchaser && (
+                  <button
+                    onClick={() =>
+                      purchaseStand(item.purchaserFunction, item.purchaserPrice)
+                    }>
+                    Purchase {item.name} Buyer
+                  </button>
+                )}
             </li>
           ))}
         </ul>
