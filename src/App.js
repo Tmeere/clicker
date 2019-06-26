715a78f3f2295ff2a6b1ee4b464e295a48068512
Thomas Meere <thomas.meere@piksel.com>
2019-06-26 11:41:34 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [money, setMoney] = useState(100);
+  const [hasLemonade, setHasLemonade] = useState(false);
+  const [hasIceCream, setHasIceCream] = useState(false);
+
+  const items = [
+    {
+      purchased: hasLemonade,
+      purchaseFunction: setHasLemonade,
+      imageUrl: '/lemonade.jpg',
+      standPrice: 100,
+      itemPrice: 3,
+      name: 'Lemonade Stand'
+    },
+    {
+      purchased: hasIceCream,
+      purchaseFunction: setHasIceCream,
+      imageUrl: '/ice cream.jpg',
+      standPrice: 150,
+      itemPrice: 10,
+      name: 'Ice Cream Stand'
+    }
+  ];
+
+  const sell = (num = 1) => {
+    setMoney(money + num);
+  }
+
+  const purchaseStand = (func, price) => {
+    func(true);
+    setMoney(money - price);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {money}
+
+        <ul>
+          {items.map((item) => (
+            <li>
+              {item.purchased && (
+                <img src={item.imageUrl} onClick={() => sell(item.itemPrice)} />
+              )}
+
+              {money >= item.standPrice && !item.purchased && (
+                <button onClick={() => purchaseStand(item.purchaseFunction, item.standPrice)}>Purchase {item.name}</button>
+              )}
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
