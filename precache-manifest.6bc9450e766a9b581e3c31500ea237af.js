cd103dee614f1ce3cf210a90af05808a13fbd987
Thomas Meere <thomas.meere@piksel.com>
2019-06-26 11:43:33 +0100

    Updates

---
+++ precache-manifest.6bc9450e766a9b581e3c31500ea237af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a76492f8055a2a9f8c016659f89aa7b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1067616aca5b439633c",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "d1067616aca5b439633c",
+    "url": "/static/js/main.101f56d2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
