a5fb740e6e8f939ff71093a5a32409df88c5c67b
Thomas Meere <thomas.meere@piksel.com>
2019-06-26 12:17:42 +0100

    things

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
   const [money, setMoney] = useState(100);
   const [hasLemonade, setHasLemonade] = useState(false);
+  const [hasLemonadePurchaser, setHasLemonadePurchaser] = useState(false);
   const [hasIceCream, setHasIceCream] = useState(false);
-
   const items = [
     {
       purchased: hasLemonade,
       purchaseFunction: setHasLemonade,
-      imageUrl: '/lemonade.jpg',
+      purchaserFunction: setHasLemonadePurchaser,
+      purchaserPurchased: hasLemonadePurchaser,
+      imageUrl: 'lemonade.jpg',
       standPrice: 100,
+      purchaserPrice: 50,
       itemPrice: 3,
       name: 'Lemonade Stand'
     },
     {
       purchased: hasIceCream,
       purchaseFunction: setHasIceCream,
-      imageUrl: '/ice cream.jpg',
+      imageUrl: 'ice cream.jpg',
       standPrice: 150,
       itemPrice: 10,
       name: 'Ice Cream Stand'
     }
   ];
+  
+  useEffect(() => {
+    if (hasLemonadePurchaser) {
+      const purchaseItem = setInterval(() => {
+        setMoney(money + 3);
+      }, 1000);
 
+      return () => {
+        clearInterval(purchaseItem)
+      }
+    }
+  }, [hasLemonadePurchaser, money]);
+  
   const sell = (num = 1) => {
     setMoney(money + num);
   }
@@ -48,6 +63,10 @@ function App() {
 
               {money >= item.standPrice && !item.purchased && (
                 <button onClick={() => purchaseStand(item.purchaseFunction, item.standPrice)}>Purchase {item.name}</button>
+              )}
+
+              {item.purchased && money >= item.purchaserPrice && !item.purchaserPurchased && (
+                <button onClick={() => purchaseStand(item.purchaserFunction, item.purchaserPrice)}>Purchase {item.name} Buyer</button>
               )}
             </li>
           ))}
