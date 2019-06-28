dfc4ed9e95f4559513502b3495c435d271a8d074
Joseph Abell <joseph.abell@piksel.com>
2019-06-28 16:02:10 +0100

    remove unneeded const on purchaser

--- src/components/Purchaser.js
+++ src/components/Purchaser.js
@@ -1,38 +1,18 @@
-import React from "react";
-import { css } from "emotion";
-import Button from "./Button";
+import React from 'react';
+import Button from './Button';
 
 const Purchaser = ({
-  name = "",
+  name = '',
   hasItem = 0,
   money = 0,
   purchaserPrice = 0,
   setHasPurchaser = () => {},
   hasPurchaser = 0,
   purchase = () => {},
-  highlightColour = "",
-  lowlightColour = "",
-  textColour = ""
+  highlightColour = '',
+  lowlightColour = '',
+  textColour = '',
 }) => {
-  const buttonStyles = css`
-    font-family: -apple-system, 'BlinkMacSystemFont', 'Segoe UI', 'Roboto',
-      'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
-      'Helvetica Neue', sans-serif;
-    background: ${highlightColour};
-    border: 1px solid ${lowlightColour};
-    margin-bottom: 20px !important;
-    width: 250px;
-    display: block;
-    color: ${textColour};
-    transition: color 0.3s, background: 0.3s, border 0.3s;
-
-    &:disabled {
-      background: #ddd;
-      border: 1px solid #bbb;
-      color: #bbb;
-    }
-  `;
-
   const disabled = money < purchaserPrice || hasItem === 0;
 
   const handleButtonClick = () => {
@@ -48,7 +28,7 @@ const Purchaser = ({
         lowlightColour={lowlightColour}
         textColour={textColour}
         disabled={disabled}
-        fullWidth={fullWidth}
+        fullWidth={true}
       />
     </li>
   );
